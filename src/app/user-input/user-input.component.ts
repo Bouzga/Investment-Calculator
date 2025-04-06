0c73b2dc7c84e630d6039cbd0761dc8f4178cd9f
Bouzga <bouzgaoumnia@gmail.com>
2025-04-06 00:11:23 +0000

    add signal

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InvestmentInput } from '../investment-input.model';
 
@@ -12,18 +12,18 @@ import { InvestmentInput } from '../investment-input.model';
 export class UserInputComponent {
 
   @Output() calculate = new EventEmitter<InvestmentInput>();
-  entredInitialInvestment = '';
-  entredAnnualInvestment = '';
-  entredExpectedReturn = '5';
-  entredDuration = '10';
+  entredInitialInvestment = signal ('0');
+  entredAnnualInvestment = signal ('0');
+  entredExpectedReturn = signal ('5');
+  entredDuration = signal ('10');
 
   onSubmit() {
     console.log('Form submitted!');
     this.calculate.emit({
-      initialInvestment: +this.entredInitialInvestment, 
-        annualInvestment: +this.entredAnnualInvestment, 
-        expectedReturn: +this.entredExpectedReturn, 
-        duration: +this.entredDuration,
+      initialInvestment: +this.entredInitialInvestment(), 
+        annualInvestment: +this.entredAnnualInvestment(), 
+        expectedReturn: +this.entredExpectedReturn(), 
+        duration: +this.entredDuration(),
     });
   }
 }
